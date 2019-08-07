21d0a2acb55b45f7f56d3fda48174be3b6c03125
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-08-07 13:55:24 +0100

    new colour

--- atools/plotting.py
+++ atools/plotting.py
@@ -22,7 +22,7 @@ def colors_i_like():
     """
     return [
         '#FA7268', '#F8A72A', '#DAF7A6', '#900C3F', '#6BADB0',
-        '#DB869D', '#F6D973'
+        '#DB869D', '#F6D973', 'mediumvioletred'
     ]
 
 
