a20bcccfbfa7b47a7625137cba3a83ec82394548
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-11-08 11:03:55 +0000

    refactor

--- atools/stk_f.py
+++ atools/stk_f.py
@@ -884,3 +884,51 @@ def calculate_ligand_distortion(mol, cage_name, free_ligand_name=None):
     ])
 
     return NN_avg_cage_min_free, bite_avg_cage_min_free
+
+
+def calculate_N_COM_N_angle(bb):
+    """
+    Calculate the N-COM-N angle of a ditopic building block.
+
+    This function will not work for cages built from FGs other than
+    metals + pyridine_N_metal.
+
+    Parameters
+    ----------
+    bb : :class:`stk.BuildingBlock`
+        stk molecule to analyse.
+
+    Returns
+    -------
+    angle : :class:`float`
+        Angle between two bonding vectors of molecule.
+
+    """
+
+    fg_counts = 0
+    fg_positions = []
+    for fg in bb.func_groups:
+        if 'pyridine_N_metal' == fg.fg_type.name:
+            fg_counts += 1
+            # Get geometrical properties of the FG.
+            # Get N position - deleter.
+            N_position = bb.get_center_of_mass(
+                atom_ids=fg.get_deleter_ids()
+            )
+            fg_positions.append(N_position)
+
+    if fg_counts != 2:
+        sys.exit(
+            f'{bb} does not have 2 pyridine_N_metal functional '
+            'groups.'
+        )
+
+    # Get building block COM.
+    COM_position = bb.get_center_of_mass()
+
+    # Get vectors.
+    fg_vectors = [i-COM_position for i in fg_positions]
+
+    # Calculate the angle between the two vectors.
+    angle = np.degrees(angle_between(*fg_vectors))
+    return angle
