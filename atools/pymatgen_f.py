d5b74be0cfd8f008540898b7ae3e2ef748e7ab65
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-01-30 21:23:14 +0000

    find sites with certain element type

--- atools/pymatgen_f.py
+++ atools/pymatgen_f.py
@@ -10,8 +10,34 @@ Author: Andrew Tarzia
 Date Created: 30 Jan 2020
 """
 
+def get_element_sites(molecule, atomic_no):
+    """
+    Get the index of sites in the molecule with the desired element.
+
+    Parameters
+    ----------
+    molecule : :class:`pymatgen.Molecule`
+        Molecule to get sites of.
+
+    atomic_no : :class:`int`
+        Atomic number of desired element.
+
+    Returns
+    -------
+    idxs : :class:`list` of :class:`int`
+        List of site indices.
+
+    """
+
+    idxs = []
+
+    for i, atom in enumerate(molecule.sites):
+        Z = atom.specie.Z
+        if Z == atomic_no:
+            idxs.append(i)
 
 def calculate_site_order_values(structure, site):
+    return idxs
     """
     Calculate order parameters around metal centres.
 
