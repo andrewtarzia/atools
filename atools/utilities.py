ab83d0ace594266811086a43abab65677e4d468f
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-07-08 21:14:44 +0100

    Allow for multiple ETKDG versions

--- atools/utilities.py
+++ atools/utilities.py
@@ -39,21 +39,42 @@ def update_from_rdkit_conf(stk_mol, rdk_mol, conf_id):
     return stk_mol.with_position_matrix(pos_mat)
 
 
-def build_conformers(mol, N):
+def build_conformers(mol, N, ETKDG_version=None):
     """
     Convert stk mol into RDKit mol with N conformers.
+
+    ETKDG_version allows the user to pick their choice of ETKDG params.
+
+    `None` provides the settings used in ligand_combiner and unsymm.
+
+    Other options:
+        `v3`:
+            New version from DOI: 10.1021/acs.jcim.0c00025
+            with improved handling of macrocycles.
 
     """
     molecule = mol.to_rdkit_mol()
     molecule.RemoveAllConformers()
 
-    cids = rdkit.EmbedMultipleConfs(
-        mol=molecule,
-        numConfs=N,
-        randomSeed=1000,
-        useExpTorsionAnglePrefs=True,
-        useBasicKnowledge=True,
-        numThreads=4,
-    )
+    if ETKDG_version is None:
+        cids = rdkit.EmbedMultipleConfs(
+            mol=molecule,
+            numConfs=N,
+            randomSeed=1000,
+            useExpTorsionAnglePrefs=True,
+            useBasicKnowledge=True,
+            numThreads=4,
+        )
+
+    elif ETKDG_version == 'v3':
+        params = rdkit.ETKDGv3()
+        params.numThreads = 4
+        params.randomSeed = 1000
+        cids = rdkit.EmbedMultipleConfs(
+            mol=molecule,
+            numConfs=N,
+            params=params
+        )
+
     print(f'there are {molecule.GetNumConformers()} conformers')
     return cids, molecule
