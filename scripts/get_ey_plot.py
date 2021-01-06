578afe47698f40fbaeaea458922e25bad4daa01f
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-01-06 10:28:07 +0000

    Clean up g16 code.

--- scripts/get_ey_plot.py
+++ scripts/get_ey_plot.py
@@ -1,5 +1,4 @@
 import os
-#import matplotlib.pyplot as plt
 import sys
 
 
@@ -7,7 +6,7 @@ def main():
     file = sys.argv[1]
     out_file = file.replace('.log', '.grep')
 
-    grep_cmd = f"grep 'RB3LYP)' {file} > {out_file}"
+    grep_cmd = f"grep 'RB3LYP)' {file} | grep 'A.U.' > {out_file}"
     os.system(grep_cmd)
 
     with open(out_file, 'r') as f:
@@ -15,18 +14,11 @@ def main():
 
     vals = [float(i.split('  ')[2]) for i in lines]
     print(f'current energy = {vals[-1]}')
-    print(f'minimum energy of {min(vals)} at {vals.index(min(vals))+1} of {len(vals)}')
-
- #   fig, ax = plt.subplots()
-  #  ax.plot([i-min(vals) for i in vals], c='k')
-   # ax.tick_params(axis='both', which='major', labelsize=16)
-    #ax.set_xlabel('step', fontsize=16)
-    #ax.set_ylabel('energy', fontsize=16)
-    #fig.tight_layout()
-    #fig.savefig("energy_plot.pdf", dpi=720, bbox_inches='tight')
-    #plt.close()
+    print(
+        f'minimum energy of {min(vals)} at '
+        f'{vals.index(min(vals))+1} of {len(vals)}'
+    )
 
 
 if __name__ == '__main__':
     main()
-
